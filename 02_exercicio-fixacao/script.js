bcfcac73515d5b946e620bf35f856bebe831ac35
christhian brandão <christhianbrandao69@gmail.com>
2022-07-20 21:13:30 -0300

    aula operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,21 @@
 // EXERCICIO 01
+
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+/*let idade1 = prompt("qual a sua idade?")
+let idade2 = prompt("qual a idade do seu melhor amigo?")
+console.log('a difença é de', Number(idade1) - Number(idade2))*/
+
+
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+
+// !a && b
+// (b && a) && !d
+// a || d
+// c && !d
